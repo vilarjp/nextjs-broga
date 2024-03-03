48361dd022d1719d493d77a2c30dd855dac0e922
João Paulo Vilar <joao@MacBook-Pro-de-Joo.local>
2024-03-02 21:39:59 -0300

    feat: add Navbar icons from iconfinder.com

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { AwardIcon } from "./AwardIcon";
+export { GamepadIcon } from "./GamepadIcon";
+export { HomeIcon } from "./HomeIcon";
+export { RouteIcon } from "./RouteIcon";
+export { UserIcon } from "./UserIcon";
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,14 @@
+import {
+  AwardIcon,
+  GamepadIcon,
+  HomeIcon,
+  RouteIcon,
+  UserIcon,
+} from "@/components/Icons";
+
 export const Navbar = () => {
   return (
-    <nav className="flex h-screen flex-col gap-4 bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2">
+    <nav className="flex h-screen flex-col gap-4 bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2 text-slate-300">
       <div>
         <img
           src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png"
@@ -9,14 +17,26 @@ export const Navbar = () => {
         />
       </div>
       <ul className="my-4 flex-grow border-t border-indigo-400/20 hover:border-indigo-400/40">
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Home</li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Games</li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Top 10</li>
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Walkthroughs</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 hover:text-indigo-500 cursor-pointer flex gap-2 items-center">
+          <HomeIcon className="w-4 h-4" />
+          Home
+        </li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 hover:text-indigo-500 cursor-pointer flex gap-2 items-center">
+          <GamepadIcon className="w-4 h-4" /> Games
+        </li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 hover:text-indigo-500 cursor-pointer flex gap-2 items-center">
+          <AwardIcon className="w-4 h-4" />
+          Top 10
+        </li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 hover:text-indigo-500 cursor-pointer flex gap-2 items-center">
+          <RouteIcon className="w-4 h-4" /> Walkthroughs
+        </li>
       </ul>
       <ul className="my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
-        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">User</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 hover:text-indigo-500 cursor-pointer flex gap-2 items-center">
+          <UserIcon className="w-4 h-4" /> User
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
