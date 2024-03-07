282a6c443a7bb4f80738491199ec8ecba0cb17fb
João Paulo Vilar <vilarjp93@gmail.com>
2024-03-07 20:41:16 -0300

    feat: add articles mocks and some css transition improvements

--- src/components/Navbar/NavBarListItem.tsx
+++ src/components/Navbar/NavBarListItem.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react";
 
 export const NavbarListItem = ({ children }: ComponentProps<"li">) => {
   return (
-    <li className="rounded-lg bg-transparent hover:bg-slate-800 hover:text-fuchsia-400 cursor-pointer flex gap-2 items-center">
+    <li className="rounded-lg bg-transparent hover:bg-slate-800 transition duration-300 hover:text-fuchsia-400 cursor-pointer flex gap-2 items-center">
       {children}
     </li>
   );
