30357c109f694918c81de1c93706109affdb3b5f
João Paulo Vilar <vilarjp93@gmail.com>
2024-03-09 16:55:44 -0300

    feat: add articles service

---
+++ src/services/articles.ts
@@ -0,0 +1,42 @@
+import Article from "@/libs/database/articles";
+
+const HIGHLIGHT_ARTICLES_COUNT = 4;
+
+export default {
+  getArticles: async ({ page = 1, limit = 10 }) => {
+    const offset = (page - 1) * limit;
+
+    const data = await Article.get({ offset, limit });
+    const total = await Article.count({});
+
+    return {
+      data,
+      metadata: {
+        page,
+        limit,
+        offset,
+        total,
+      },
+    };
+  },
+  getHomeArticles: async ({ page = 1, limit = 10 } = {}) => {
+    const offset = (page - 1) * limit + HIGHLIGHT_ARTICLES_COUNT;
+
+    const data = await Article.get({
+      offset,
+    });
+
+    return {
+      data,
+    };
+  },
+  getHighlightArticles: async () => {
+    const data = await Article.get({
+      limit: HIGHLIGHT_ARTICLES_COUNT,
+    });
+
+    return {
+      data,
+    };
+  },
+};
